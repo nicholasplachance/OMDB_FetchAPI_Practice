82892cdb4a1423f39b847004b30b080788210af5
nick lachance <nicholasplachance@gmail.com>
2019-10-08 16:57:35 -0500

    Add an array of movies with imdb ids

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const superHeroMovieIds = [
   { movieName: "Iron Man", movieId: "tt0371746" },
   { movieName: "The Incredible Hulk", movieId: "tt0800080" },
-  { movieName: "Iron Man 2", movieId: "tt1228705" }
+  { movieName: "Iron Man 2", movieId: "tt1228705" },
+  { movieName: "Thor", movieId: "tt0800369"},
+  { movieName: "Captain America: The First Avenger", movieId: "tt0458339"},
+  { movieName: "The Avengers", movieId: "tt0848228"},
+  { movieName: "Iron Man 3", movieId: "tt1300854"},
+  { movieName: "Thor: The Dark World", movieId: "tt1981115"},
+  { movieName: "Captain America: The Winter Soldier", movieId: "tt1843866"},
+  { movieName: "Guardians of the Galaxy", movieId: "tt2015381"},
+  { movieName: "Avengers: Age of Ultron", movieId: "tt2395427"},
+  { movieName: "Ant-Man", movieId: "tt2395427"}
 ];
 
 const movieCard = document.querySelector("#movie-card");
 
-const movie = superHeroMovieIds[1].movieId;
+const movie = superHeroMovieIds[5].movieId;
 const apiKey = `31152d26`;
 const url = `http://www.omdbapi.com/?apikey=${apiKey}&i=${movie}`;
 
